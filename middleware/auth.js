8892273e5c399c9b94f7905faf9058c3ca2ec812
KTC-HirokiNomura <hiroki.nomura@kinto-technologies.com>
2026-04-18 23:52:59 +0900

    refactor: switch auth middleware to jwt

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,23 @@
 'use strict';
 
-function requireAuth(req, res, next) {
-  if (req.session && req.session.userId) {
-    return next();
+const jwtService = require('../services/jwtService');
+
+function authContext(req, res, next) {
+  const token = req.cookies?.[jwtService.COOKIE_NAME];
+  if (token) {
+    const payload = jwtService.verify(token);
+    if (payload) {
+      req.user = { id: payload.sub, email: payload.email, username: payload.username };
+      res.locals.userEmail = payload.email || payload.username;
+    }
   }
+  if (!res.locals.userEmail) res.locals.userEmail = null;
+  next();
+}
+
+function requireAuth(req, res, next) {
+  if (req.user) return next();
   res.redirect('/auth/login');
 }
 
-module.exports = { requireAuth };
+module.exports = { authContext, requireAuth };
